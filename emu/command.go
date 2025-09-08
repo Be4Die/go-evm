6be5ea491e5c96b35d83ef4e95f31a3916e55ef0
Be4Die <misha375375@gmail.com>
2025-09-08 16:31:35 +0400

    Create command.go

---
+++ emu/command.go
@@ -0,0 +1,7 @@
+package emu
+
+// Command интерфейс представляет исполняемую команду виртуальной машины
+type Command interface {
+    // Execute выполняет команду используя предоставленные CPU и Memory
+    Execute(cpu CPU, memory Memory) error
+}
