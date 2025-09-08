169a5c32e7ca3a3350e92d5844fbbbccf3c2abdc
Be4Die <misha375375@gmail.com>
2025-09-08 16:50:08 +0400

    change cpu abstractions

--- vm/cpu.go
+++ vm/cpu.go
@@ -4,13 +4,16 @@ package vm
 type CPU interface {
     // GetRegister возвращает текущее значение указанного регистра
     GetRegister(registerType RegisterType) (Word, error)
-    
+
     // SetRegister устанавливает значение указанного регистра
     SetRegister(registerType RegisterType, value Word) error
-    
+
     // Run начинает выполнение программного кода
     Run() error
-    
+
+    // Step выполняет одну следующую инструкцию
+    Step() error
+
     // Reset сбрасывает состояние процессора в начальное положение
     Reset() error
 }
