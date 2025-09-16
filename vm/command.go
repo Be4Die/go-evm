66667ff465c545e6ab6e061e749b26bd0d65db49
Be4Die <misha375375@gmail.com>
2025-09-16 04:28:43 +0400

    global rewrite with concrete variant
    remove old source files and create docs files

--- vm/command.go
+++
@@ -1,7 +0,0 @@
-package vm
-
-// интерфейс представляет исполняемую команду виртуальной машины
-type Command interface {
-    // Execute выполняет команду используя предоставленные CPU и Memory
-    Execute(cpu CPU, memory Memory) error
-}
